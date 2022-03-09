d9688fbb48d34d1a1be15e79742be8d0f0a4faa4
Merciall <merciallgroup@gmail.com>
2022-03-09 10:35:36 -0500

    Added Str cast so that strings can be defaulted to Str objects on get

---
+++ src/App/Casts/Str.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Merciall\Merci\App\Services\Str as ServicesStr;
+use Merciall\Merci\Facades\Merci;
+use ValueError;
+
+class Str implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return array
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        return Merci::Str($value);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  array  $value
+     * @param  array  $attributes
+     * @return string
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        if (!$value instanceof ServicesStr && !is_string($value))
+            throw new ValueError("The given value not an instance of a string or a Str instance");
+
+        return (string) $value;
+    }
+}
